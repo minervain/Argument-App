ad475112a8421e8dd738320495e39352001b9b42
minervain <aliosmanucar987@gmail.com>
2024-01-06 22:08:45 +0300

    apiError message

--- server/src/main/java/com/argument/aou/user/UserController.java
+++ server/src/main/java/com/argument/aou/user/UserController.java
@@ -1,10 +1,15 @@
 package com.argument.aou.user;
 
+import com.argument.aou.Error.ApiError;
 import com.argument.aou.Shared.GenericMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -12,8 +17,19 @@ public class UserController {
     @Autowired
     UserService userService;
     @PostMapping("/api/v1/users")
-    GenericMessage createUser (@RequestBody User user){
+    ResponseEntity<?> createUser (@RequestBody User user){
+        if(user.getUsername()==null || user.getUsername().isEmpty()){
+            ApiError apiError =new ApiError();
+            apiError.setPath("/api/v1/users");
+            apiError.setMessage("Validation errors");
+            apiError.setStatus(400);
+            Map<String,String> validationErrors=new HashMap<>();
+            validationErrors.put("username","null");
+
+
+            return ResponseEntity.badRequest().body(apiError);
+        }
             userService.save(user);
-            return new GenericMessage("User Oluşturuldu");
+            return ResponseEntity.ok(new GenericMessage("User Oluşturuldu"));
     }
 }
